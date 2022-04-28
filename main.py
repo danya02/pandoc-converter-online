4b51eb1712e1ede0cb54adcaec71c7e51667c55d
Danya Generalov <danya@danya02.ru>
2022-04-28 23:10:01 +0300

    Adding basic upload and captcha checking.

--- main.py
+++ main.py
@@ -1,10 +1,27 @@
-from flask import Flask
+from flask import Flask, render_template, request, redirect, url_for, flash
+from flask_hcaptcha import hCaptcha
+from dotenv import load_dotenv
+import os
+load_dotenv('conf.env')
+
 
 app = Flask(__name__)
+app.config['HCAPTCHA_ENABLED'] = True
+app.config['HCAPTCHA_SITE_KEY'] = os.getenv('HCAPTCHA_SITE_KEY')
+app.config['HCAPTCHA_SECRET_KEY'] = os.getenv('HCAPTCHA_SECRET_KEY')
+app.secret_key = os.getenv('FLASK_SECRET_KEY')
+hcaptcha = hCaptcha(app)
 
 @app.route('/')
 def index():
-    return 'Hello World!'
+    return render_template('index.html')
+
+@app.route('/convert', methods=['POST'])
+def convert():
+    if not hcaptcha.verify():
+        flash('Invalid captcha')
+        return redirect(url_for('index'))
+    return 'Submitted!'
 
 if __name__ == '__main__':
     app.run('0.0.0.0', 5000)
